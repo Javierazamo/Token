381bc33ea929cbc9d7517db7b89bfecd5de49cf7
Javierazamo <jazamogon@gmail.com>
2023-08-18 11:21:29 -0400

    prime commit

--- pedir_token.py
+++ pedir_token.py
@@ -2,61 +2,56 @@ import requests
 import json
 import conf
 
-
 def pedir_token(usuario, clave):
-url = "https://sandboxapicdc.cisco.com/api/aaaLogin.json"
-
-data = {
-    "aaaUser" : {
-        "attributes" : {
-            "name" : "usuario",
-            "pwd" : "clave"
+    url = "https://10.10.20.14/api/aaaLogin.json"
+    data = {
+        "aaaUser": {
+            "attributes": {
+                "name": usuario,
+                "pwd": clave
+            }
         }
     }
-}
+    cabecera = {
+        "Content-Type": "application/json"
+    }
 
-cabecera = {"content-type":"application/json" }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.post(url, data=json.dumps(data), headers=cabecera, verify=False)
+    respuesta_json = respuesta.json()
+    token = respuesta_json["imdata"][0]["aaaLogin"]["attributes"]["token"]
+    return token
 
+token = pedir_token(conf.usuario, conf.clave)
+print("Token: " + token)
 
-requests.packages.urllib3.disable_warnings()
-respuesta = requests.post(url,json.dumps(data),headers=cabecera, verify= False)
-respuesta_json=respuesta.json()
-print(respuesta.json())
-
-API_Token = respuesta_json["indat"][0]["aaaLogin"]["attributes"]["token"]
-print("hola")
-return token
-
-token = pedir_token(conf.usuario,conf.clave)
-print("token: "+token)
-# GET https://apic-ip-address/api/classtopSystem.json"
-url1 = "https://sandboxapicdc.cisco.com/api/class/topSystem.json"
+url1 = "https://10.10.20.14/api/class/topSystem.json"
+#GET http://apic-ip-address/api/class/topSystem.json"
 cabecera = {
-  "content-Type": "application/json"
-}
+    "Content-Type": "application/json"}
 WebToken = {
-  "APIC-Cookie": pedir_token(conf.usuario,conf.clave)
+    "APIC-Cookie": pedir_token(conf.usuario, conf.clave)
+
 }
 
 try:
     respuesta1 = requests.get(url1, headers=cabecera, cookies=WebToken,verify=False)
 except Exception as err:
-   print ("Error al pedir el API")
-   exit (1)
+    print("Error al pedir el API")
+    exit(1)
 
+respuesta1_json = respuesta1.json()
+print(respuesta1_json)
 
-respuesta1 = requests.get(url, headers=cabecera, cookies=WebToken,verify=False)
-print(respuesta1.jason())
-
-for i in range(0,int(respuesta1.json()["totalCount"]))
-    #print("ip : " + respuesta1.json()["imdata"]["i"]["topSystem"]["attributes"]["address"])
-    ip_local = respuesta1.json()["imdata"]["i"]["topSystem"]["attributes"]["address"]
-    mac_fabric = respuesta1.json()["imdata"]["i"]["topSystem"]["attributes"]["address"]
-    estado = respuesta1.json()["imdata"]["i"]["topSystem"]["attributes"]["state"]
-    print("IP: "+ip_local+" / "+mac_fabric+" / "+estado)
+for i in range(0,4):
+    print("ip : "+respuesta1.json()["imdata"][i]["topSystem"]["attributes"]["address"])
+    ip_local = respuesta1_json["imdata"][i]["topSystem"]["attributes"]["address"]
+    mac_fabric = respuesta1_json["imdata"][i]["topSystem"]["attributes"]["fabricMAC"]
+    estado = respuesta1_json["imdata"][i]["topSystem"]["attributes"]["state"]
+    print("IP:", ip_local, "/", mac_fabric, "/", estado)
 
 print(respuesta1.request.url)
 print(respuesta1.request.method)
-print(respuesta1.request.headers["Cookies"])
+print(respuesta1.request.headers["Cookie"])
 
-print(respuesta1.headers["server"])
+print(respuesta1.headers["Server"])
